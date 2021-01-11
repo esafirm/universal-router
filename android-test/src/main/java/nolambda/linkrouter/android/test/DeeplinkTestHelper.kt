f6e5596cbc0bedfde85d5dc2da61d9a4d49651c5
Esa Firman <esafirm21@gmail.com>
2021-01-11 19:10:48 +0700

    should throw in deeplink assert

--- android-test/src/main/java/nolambda/linkrouter/android/test/DeeplinkTestHelper.kt
+++ android-test/src/main/java/nolambda/linkrouter/android/test/DeeplinkTestHelper.kt
@@ -7,7 +7,8 @@ import nolambda.linkrouter.android.RouteParam
 data class DeepLinkAssertionData<P : Any, E>(
     val routeParam: RouteParam<P, E>?,
     val route: BaseRoute<P>,
-    val currentPath: String
+    val currentPath: String,
+    val error: Throwable? = null
 )
 
 typealias DeepLinkAssertion<P, E> = (DeepLinkAssertionData<P, E>) -> Unit
@@ -25,16 +26,22 @@ fun <P : Any, E> AbstractAppRouter<E>.testDeepLink(
 
     deepLinkData.forEach { (route, testData) ->
         var lastRouteParam: RouteParam<P, E>?
+        var error: Throwable? = null
         router.register(route) {
             lastRouteParam = it
         }
         testData.forEach { (path, assertion) ->
             lastRouteParam = null
-            router.goTo(path)
+            try {
+                router.goTo(path)
+            } catch (e: Throwable) {
+                error = e
+            }
             assertion(DeepLinkAssertionData(
                 routeParam = lastRouteParam,
                 route = route,
-                currentPath = path
+                currentPath = path,
+                error = error
             ))
         }
     }
@@ -43,5 +50,13 @@ fun <P : Any, E> AbstractAppRouter<E>.testDeepLink(
 object DeepLinkAssertions {
     fun <P : Any, E> shouldValid(isValid: Boolean = true): DeepLinkAssertion<P, E> = { p ->
         check(p.routeParam != null == isValid) { "DeepLink is not handled: ${p.currentPath}" }
+    }
+
+    /**
+     * This assertion only works if you're not setting a custom error handler to Router plugin
+     *
+     */
+    inline fun <reified T : Throwable> shouldThrow(): DeepLinkAssertion<*, *> = { p ->
+        check(p.error is T)
     }
 }
