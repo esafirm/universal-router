6e286ffb34cb8dc1aa525f09ed74eace181adc54
Esa Firman <esafirm21@gmail.com>
2021-02-23 11:51:43 +0700

    fix measure middleware

--- android/src/main/java/nolambda/linkrouter/android/measure/MeasureMiddleWare.kt
+++ android/src/main/java/nolambda/linkrouter/android/measure/MeasureMiddleWare.kt
@@ -1,16 +1,19 @@
 package nolambda.linkrouter.android.measure
 
-import nolambda.linkrouter.android.middlewares.MiddleWareChain
+import nolambda.linkrouter.android.BaseRoute
+import nolambda.linkrouter.android.RouteParam
+import nolambda.linkrouter.android.middlewares.MiddleWareResult
 import nolambda.linkrouter.android.middlewares.Middleware
 
 class MeasureMiddleWare<Extra>(
     private val measureConfig: MeasureConfig,
     private val middleWares: List<Middleware<Extra>>
 ) : Middleware<Extra> {
-    override fun onRouting(chain: MiddleWareChain<Extra>): MiddleWareChain<Extra> {
-        return middleWares.fold(chain) { acc, middleware ->
+    override fun onRouting(route: BaseRoute<*>, routeParam: RouteParam<*, Extra>): MiddleWareResult<Extra> {
+        val initial = MiddleWareResult(route, routeParam)
+        return middleWares.fold(initial) { acc, middleware ->
             measureConfig.doMeasure("$middleware") {
-                middleware.onRouting(acc)
+                middleware.onRouting(acc.route, acc.routeParam)
             }
         }
     }
