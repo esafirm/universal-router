ce44a2af155579a093ad9d5d635e9c43b59c365f
Esa Firman <esafirm21@gmail.com>
2021-02-23 18:29:41 +0700

    update setup script

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 
 rm settings.gradle
 echo "include ':core', ':android', ':annotations', ':processor', ':android-test" >> settings.gradle
+echo "include ':extras:android-fragment', ':extras:android-activityresult'" >> settings.gradle
 
 cat settings.gradle
