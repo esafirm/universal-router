04f7859b3ceea965001e51a194af089fa9226d96
Esa Firman <esafirm21@gmail.com>
2021-02-24 10:22:03 +0700

    release preparation

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 rm settings.gradle
-echo "include ':core', ':android', ':annotations', ':processor', ':android-test" >> settings.gradle
+echo "include ':core', ':android', ':annotations', ':processor', ':android-test'" >> settings.gradle
 echo "include ':extras:android-fragment', ':extras:android-activityresult'" >> settings.gradle
 
 cat settings.gradle
